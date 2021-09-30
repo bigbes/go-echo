9a9922f71dd49398685b16966a9bd62667ea09b0
Sergey Morozov <ita.sammann@gmail.com>
2021-09-30 18:23:30 +0200

    Add JSON body

--- main.go
+++ main.go
@@ -7,22 +7,29 @@ import (
 )
 
 type request struct {
-	URL     string      `json:"url"`
-	Method  string      `json:"method"`
-	Headers http.Header `json:"headers"`
-	Body    []byte      `json:"body"`
+	URL      string                 `json:"url"`
+	Method   string                 `json:"method"`
+	Headers  http.Header            `json:"headers"`
+	Body     string                 `json:"body"`
+	BodyJSON map[string]interface{} `json:"body_json,omitempty"`
 }
 
 func handle(rw http.ResponseWriter, r *http.Request) {
-	var err error
 	rr := &request{}
 	rr.Method = r.Method
 	rr.Headers = r.Header
 	rr.URL = r.URL.String()
-	rr.Body, err = ioutil.ReadAll(r.Body)
+	body, err := ioutil.ReadAll(r.Body)
 	if err != nil {
 		http.Error(rw, err.Error(), http.StatusInternalServerError)
 		return
+	}
+	rr.Body = string(body)
+
+	jsonBody := make(map[string]interface{})
+	err = json.Unmarshal(body, &jsonBody)
+	if err == nil {
+		rr.BodyJSON = jsonBody
 	}
 
 	rrb, err := json.Marshal(rr)
@@ -33,6 +40,7 @@ func handle(rw http.ResponseWriter, r *http.Request) {
 
 	rw.Header().Set("Content-Type", "application/json")
 	rw.Write(rrb)
+	rw.Write([]byte("\n"))
 }
 
 func main() {
